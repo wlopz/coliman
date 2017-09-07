fe6f7a2641cc2eb0876ae3ccedf62dc3277e2c66
Wilfredo Lopez <jrlopez85@gmail.com>
2017-09-07 02:08:06 -0700

    Mailgun config set

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,13 +1,14 @@
 class MessageMailer < ApplicationMailer
+  require 'mailgun'
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.message_mailer.contact_me.subject
-  #
   def contact_me(message)
     @body = message.body
-
-    mail to: "dynmkstudio@gmail.com", from: message.email
+    mg_client = Mailgun::Client.new ENV['mailgun_secret_api_key']
+    message_params = {:from => message.email,
+                      :to => 'dynmkstudio@gmail.com',
+                      :subject => 'Contact Form',
+                      :text => message.body}
+    mg_client.send_message ENV['mailgun_domain'], message_params
+    # send
   end
-end
+ end
