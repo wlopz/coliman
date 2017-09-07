70482d65585eef4c084bd3b58866b03668178187
Wilfredo Lopez <jrlopez85@gmail.com>
2017-09-07 13:00:39 -0700

    Corrected environment api key in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
-      api_key: 'ENV["MAILGUN_SECRET_API_KEY"]',
+      api_key: ENV["MAILGUN_SECRET_API_KEY"],
       domain: 'coliman.herokuapp.com',
   }
 
